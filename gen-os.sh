c78a5995e4d505733bfbbba17738e45a74f3f063
Laurent Kappel <laurentx.kappel@intel.com>
2012-05-25 11:38:00 +0200

    script must handle errors raised by gen-os.sh (android-release-ext)
    BZ: 38400
    
    Change-Id: Iad0c8f0f9006c90276f99047f458a43d881d445f
    Signed-off-by: Laurent Kappel <laurentx.kappel@intel.com>
    Reviewed-on: http://android.intel.com:8080/50183
    Reviewed-by: Dutta, Ranjan <ranjan.dutta@intel.com>
    Tested-by: Dutta, Ranjan <ranjan.dutta@intel.com>

--- gen-os.sh
+++ gen-os.sh
@@ -1,23 +1,28 @@
-#!/bin/bash
+#!/bin/bash -x
 
-abs_path()
+set -e
+
+function abs_path()
 {
-        rel_path=`dirname $1`;
-        cd $rel_path;
-        abs_path=`pwd`
-        cd - 2>&1 > /dev/null
-        echo $abs_path;
+    rel_path=`dirname $1`;
+    cd $rel_path;
+    abs_path=`pwd`
+    cd - 2>&1 > /dev/null
+    echo $abs_path;
+
+    return 0
 }
 
-usage()
+function usage()
 {
-        echo "$0 --input <OS image> --output <Stitched OS image output> --xml <XML filename>"
-        exit 1
+    echo "$0 --input <OS image> --output <Stitched OS image output> --xml <XML filename>"
+
+    return 0
 }
 
 TEMP=`getopt \
-         -o i:o:x: \
-         --long input:,output:,xml: \
+         -o hi:o:x: \
+         --long help,input:,output:,xml: \
          -n '$0' \
          --  "$@"`
 
@@ -25,18 +30,19 @@ eval set -- "$TEMP"
 while true ; do
     echo "+++++ case $1 +++++"
     case "$1" in
-        --input) OS="$2"; shift 2;;
-        --output) OSUSB="$2"; shift 2;;
-        --xml) XML="$2"; shift 2;;
+        -i|--input) OS="$2"; shift 2;;
+        -h|--help) usage; exit 0; shift;;
+        -o|--output) OSUSB="$2"; shift 2;;
+        -x|--xml) XML="$2"; shift 2;;
         --) shift ; break ;;
     esac
 done
 
 _EXIT=0
 
-[ -z $OS ] && usage;
-[ -z $OSUSB ] && usage;
-[ -z $XML ] && usage;
+[ -z $OS ] && usage >&2 && exit 1;
+[ -z $OSUSB ] && usage >&2 && exit 1;
+[ -z $XML ] && usage >&2 && exit 1;
 
 echo OS=$OS
 echo OSUSB=$OSUSB
@@ -63,10 +69,7 @@ sed -r -i -e 's;ImageName\s*=.*$;ImageName = '"${OSUSB}"';g' $TMP_DIR/$OS_CFG
 
 # call stitching tool
 ./bin/xfstk-stitcher -k $TMP_DIR/$XML -c $TMP_DIR/$OS_CFG
-if [ $? -ne 0 ]
-then
-    _EXIT=1
-fi
+[ $? -ne 0 ] && _EXIT=1
 rm -rf $TMP_DIR
 
 cd $TOP

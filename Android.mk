b3c24bc4a7be6bbe2fb9f17f7007f58f85448b12
buildslave <buildslave@buildbot.tl.intel.com>
2013-08-30 13:22:52 +0000

    Automatically revert commits from previous weekly release
    Change-Id: I9e6679f2cb9affe3c413e7084c420562acaa8a7b
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- Android.mk
+++ Android.mk
@@ -1,31 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_PREBUILT_EXECUTABLES := bin/xfstk-stitcher
-include $(BUILD_HOST_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := ifcl
-LOCAL_IS_HOST_MODULE := true
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_MODULE_SUFFIX := .zip
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE):
-	$(hide) mkdir -p $(dir $@)
-	$(hide) mkdir -p $(HOST_OUT_EXECUTABLES)
-	$(hide) cp $(LOCAL_PATH)/bin/portable-fcl/$(notdir $@) $(dir $@)
-	$(hide) unzip -o $@ -d $(HOST_OUT_EXECUTABLES)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := stitcher-portable-linux
 LOCAL_IS_HOST_MODULE := true
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_MODULE_SUFFIX := .zip
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE):
-	$(hide) mkdir -p $(dir $@)
-	$(hide) mkdir -p $(HOST_OUT_EXECUTABLES)
-	$(hide) cp $(LOCAL_PATH)/bin/portable-stitcher/$(notdir $@) $(dir $@)
-	$(hide) unzip -o $@ -d $(HOST_OUT_EXECUTABLES)
+
+include $(BUILD_MULTI_PREBUILT)
